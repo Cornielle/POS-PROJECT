72860b985a65220ad097fe30a92b7539314d68ce
MacBook Pro <bcornielledev1@gmail.com>
2020-02-08 01:30:49 -0400

    saving changes in control de usuarios

--- src/Screens/Users.js
+++ src/Screens/Users.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import {View , StyleSheet} from 'react-native'
-import { DataTable, Card } from 'react-native-paper';
+import { DataTable, Card, Button,Text } from 'react-native-paper';
 import Header from '../Components/Header';
 import Search from '../Components/Search';
 import {  Block  } from 'galio-framework';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import normalize from 'react-native-normalize';
 export default class Users extends React.Component {
   render() {
@@ -17,24 +18,36 @@ export default class Users extends React.Component {
         <Card>
         <DataTable>
             <DataTable.Header>
+            <DataTable.Title>#</DataTable.Title>  
             <DataTable.Title>Nombre</DataTable.Title>
             <DataTable.Title >Rol</DataTable.Title>
             <DataTable.Title >Estado</DataTable.Title>
-            <DataTable.Title ></DataTable.Title>
             </DataTable.Header>
 
             <DataTable.Row>
             <DataTable.Cell>1</DataTable.Cell>
             <DataTable.Cell>159</DataTable.Cell>
             <DataTable.Cell>6.0</DataTable.Cell>
-            <DataTable.Cell ></DataTable.Cell>
+            <DataTable.Cell>6.0</DataTable.Cell>
+            <Icon 
+              name="ellipsis-v" 
+              size={25} 
+              color="#2c3e50" 
+              style={styles.Icon}
+              />
             </DataTable.Row>
 
             <DataTable.Row>
+            <DataTable.Cell>1</DataTable.Cell>
             <DataTable.Cell>Ice</DataTable.Cell>
             <DataTable.Cell >237</DataTable.Cell>
             <DataTable.Cell>8.0</DataTable.Cell>
-            <DataTable.Cell>159</DataTable.Cell>
+            <Icon 
+              name="ellipsis-v" 
+              size={25} 
+              color="#2c3e50" 
+              style={styles.Icon}
+              />
             </DataTable.Row>
 
             <DataTable.Pagination
@@ -52,6 +65,10 @@ export default class Users extends React.Component {
 const styles = StyleSheet.create({
   Container: {
     flex: 1,
-    backgroundColor:'#f6f6f6'
+    backgroundColor:'#f1f1f1'
   },
+  Icon:{
+    marginTop:normalize(15),
+    alignItems: 'flex-end'
+  }
 });
